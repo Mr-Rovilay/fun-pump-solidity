fdc4f4193936084b8fefae842e9455dbc972206f
ayodeji akinola <ayodejiakinola007@gmail.com>
2025-06-13 22:14:45 +0100

    jk

--- test/Factory.js
+++ test/Factory.js
@@ -1,17 +1,22 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers")
-const { expect } = require("chai")
-const { ethers } = require("hardhat")
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Factory", function () {
+  const FEE = ethers.parseUnits("0.01", 18);
 
-    it("should have a name", async function() {
-        const Factory = await ethers.getContractFactory("Factory")
+  async function deployFactoryFixture() {
+    const Factory = await ethers.getContractFactory("Factory");
+    const factory = await Factory.deploy(FEE);
+    return { factory }; // ✅ Wrap it in an object
+  }
 
-        const factory = await Factory.deploy()
-
-        const name = await factory.name()
-
-        console.log(name)
-    })
-
-})
+  describe("Deployment", function () {
+    it("should set the fee", async function () {
+      const { factory } = await loadFixture(deployFactoryFixture);
+      expect(await factory.fee()).to.equal(FEE);
+    });
+  });
+});
